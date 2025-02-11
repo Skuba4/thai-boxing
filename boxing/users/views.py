992a4959299cee332a8100c5b0e21082592e51a3
GEO <chief.skriabin2011@yandex.ru>
2025-02-11 21:15:23 +0300

    Настроил первичную регистрацию

--- boxing/users/views.py
+++ boxing/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import RegisterUserForm
+
+
+class RegUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/reg.html'
+    success_url = reverse_lazy('users:log')
+
+class Login(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'users/log.html'
