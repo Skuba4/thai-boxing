982a82b2373a5910b357901bb293fa1429e1352a
GEO <chief.skriabin2011@yandex.ru>
2025-02-15 20:34:44 +0300

    Застрял на ajax

--- boxing/referee/urls.py
+++ boxing/referee/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('my_rooms/', views.MyRooms.as_view(), name='my_rooms'),
     path('join_room/', views.JoinRoom.as_view(), name='join_room'),
     path('room/<uuid:uuid_room>/', views.DetailRoom.as_view(), name='detail_room'),
+    path('create_fight/<uuid:uuid_room>/', views.CreateFight.as_view(), name='create_fight'),
 ]
